6a0b76ad04beb4dd35e1b42a10a3f56d717ffed6
Matilda Haglund <matilda.haglund@consid.se>
2023-04-11 21:34:16 +0200

    added stop watch

--- src/Components/Ticket.jsx
+++ src/Components/Ticket.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import StopWatch from "./StopWatch";
+
 import './Ticket.css';
+
+
 
 const Ticket = (ticketName, index) => (
   <div className="Ticket-Div">
     <span>{ticketName.ticketName.input}</span>
     <br />
-    <span className="Time-Span">Tid: 22m</span>
+    <StopWatch></StopWatch>
   </div>
 );
 export default Ticket;
